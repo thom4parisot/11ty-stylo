8550ab6a8adb399a70977692c164fb081adb7b02
Thomas Parisot <thom4parisot@users.noreply.github.com>
2022-12-09 00:25:38 -0500

    Site de base

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+const markdownIt = require('markdown-it')
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addGlobalData('layout', 'default.html')
+
+  const md = new markdownIt({
+    html: true,
+    linkify: true,
+    typographer: true
+  })
+
+  eleventyConfig.addFilter('md', text => md.render(text))
+
+}
---
+++ _data/articles.js
@@ -0,0 +1,69 @@
+const EleventyFetch = require('@11ty/eleventy-fetch')
+const { gql } = require('graphql-tag')
+const { print } = require('graphql')
+
+const { STYLO_GRAPHQL_ENDPOINT: url = 'https://stylo.huma-num.fr/graphql' } = process.env
+const { STYLO_GRAPHQL_TOKEN: token } = process.env
+
+const headers = {
+  'Authorization': `Bearer ${token}`,
+  'Content-Type': 'application/json'
+}
+
+const query = gql`query {
+  articles {
+    _id
+    title
+    zoteroLink
+    createdAt
+    updatedAt
+
+    contributors {
+      user {
+        _id
+        displayName
+      }
+    }
+
+    tags {
+      _id
+      name
+      color
+    }
+
+    workingVersion {
+      bib
+      md
+      yaml
+    }
+
+    versions {
+      _id
+      name
+      version
+      revision
+      createdAt
+      updatedAt
+    }
+  }
+}`
+
+module.exports = async function() {
+  // via https://github.com/apollographql/graphql-tag/issues/206#issuecomment-489168909
+  const body = JSON.stringify({
+    query: print(query)
+  })
+
+  const { data } = await EleventyFetch(`${url}?__query=${body}`, {
+    duration: '1d',
+    type: 'json',
+    verbose: true,
+    fetchOptions: {
+      method: 'POST',
+      headers,
+      body
+    }
+  })
+
+  return data.articles
+};
